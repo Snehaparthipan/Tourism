d0fb9f619dc355db0525dd0d4a29558510937725
Snehaparthipan <snehaparthipan123@gmail.com>
2025-12-18 16:00:23 +0000

    create booking page

--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -1,9 +1,19 @@
 import Search from "../Components/Search";
 import { Routes, Route, Outlet } from "react-router-dom";
 import '../CSS/home.css'
+import API from "../Utills/API";
 
 
 export default function Home() {
+  async function getName() {
+    try {
+      await API.get("/register",{
+        username
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <>
     {/* <Navbar/> */}
@@ -15,6 +25,7 @@ export default function Home() {
       />
 
       <div className="title-box">
+        <h4 onChange={getName}>welcome back </h4>
         <h2 className="quote">Explore the Colourful World</h2>
         <div className="line"></div>
         <button className="ex-btn">Explore</button>
